7d6e4ca5ea1c275dc536f3fb086c825c3cf71d6f
SkillVerse Courses <skillverse.courses@gmail.com>
2026-08-14 19:29:47 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   server: {
-    port: 5173,
+    port: 5174,
+    strictPort: true, // Fail if 5174 is already in use
     host: true,
     open: false
   }

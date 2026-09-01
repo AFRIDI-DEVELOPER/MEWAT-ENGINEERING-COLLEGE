842fea123a4018d96ac7fcb48dd1d31f376763da
afridicoder130-lang <afridicoder130@gmail.com>
2026-09-02 01:13:07 +0530

    ,...

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import App from './App'
-import './styles/index.css'
+import App from './App.jsx'
+import './styles/index_v2.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
         <HelmetProvider>
             <BrowserRouter basename="/">
                 <App />
             </BrowserRouter>
         </HelmetProvider>
-    </React.StrictMode>
+    </StrictMode>
 )
